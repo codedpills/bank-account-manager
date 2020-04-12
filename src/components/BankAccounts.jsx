f532cd7726a4a58a3ec7b556919d3d1d6905201f
zak <codedpills@gmail.com>
2020-04-12 20:51:01 +0000

    chore: display accounts from api

--- src/components/BankAccounts.jsx
+++ src/components/BankAccounts.jsx
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import AccountInfo from "./AccountInfo";
 import { deleteAccount } from "../store/accountActions";
+import fetchAccounts from "../store/fetchAccounts";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-const BankAccounts = ({ accounts, deleteAccount }) => {
-  const bankAccounts = accounts.map((account) => {
+const BankAccounts = ({
+  accounts,
+  pending,
+  error,
+  fetchAccounts,
+  deleteAccount,
+}) => {
+  useEffect(() => {
+    fetchAccounts();
+  },[]);
+  let bankAccounts;
+  if (accounts.legnth === 0) {
+    return (bankAccounts = <p>There are no accounts yet. Add some!</p>);
+  }
+  bankAccounts = accounts.map((account) => {
     return (
-      <div className="column is-4" key={account.id}>
+      <div className="column is-4" key={account._id}>
         <AccountInfo
-          id={account.id}
-          account_type={account.account_type}
-          account_name={account.account_name}
-          account_number={account.account_number}
-          bank_name={account.bank_name}
-          bank_branch={account.bank_branch}
+          id={account._id}
+          account_type={account.accountType}
+          account_name={account.accountName}
+          account_number={account.accountNumber}
+          bank_name={account.bankName}
+          bank_branch={account.bankBranch}
           deleteAccount={deleteAccount}
         />
       </div>
     );
   });
+  const loader = (
+    <div className="has-text-centered" style={{"marginTop": "3rem"}}>
+      <Loader
+        type="Oval"
+        color="rgba(139, 137, 137, 0.7)"
+        height={50}
+        width={50}
+      />
+    </div>
+  );
+  if (pending === true) return loader;
+  console.log(error);
   return (
     <div className="container bank-accounts-container">
       <div className="columns" style={{ flexWrap: "wrap" }}>
+        {error && <div className="has-text-centered">{error}</div>}
         {bankAccounts}
       </div>
     </div>
@@ -30,10 +59,13 @@ const BankAccounts = ({ accounts, deleteAccount }) => {
 
 const mapStateToProps = (state) => ({
   accounts: state.accounts,
+  pending: state.pending,
+  error: state.error,
 });
 
 const mapDispatchToProps = {
   deleteAccount,
+  fetchAccounts,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(BankAccounts);
